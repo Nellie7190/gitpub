73f0ed4389701db7367c1eca10ead0bc38d982ed
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-09-23 01:22:50 -0400

    final result

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const drinks = require('./models/drinks')
+const foods = require('./models/foods')
 const express = require('express');
 const app = express();
 
@@ -7,15 +8,17 @@ app.get('/', (req, res) => {
     res.send(drinks)
 });
 
-app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs', {allDrinks: drinks});
+app.get('/menu', (req, res) => {
+    res.render('drinks_index.ejs', {allDrinks: drinks, allFoods: foods});
 });
 
 app.get('/drinks/:indexOfDrinksArray', (req, res) => {
     res.render('drinks_show.ejs', {drink: drinks[req.params.indexOfDrinksArray]})
 })
 
-
+app.get('/foods/:indexOfFoodsArray', (req, res) => {
+    res.render('foods_show.ejs', {food: foods[req.params.indexOfFoodsArray]})
+})
 
 app.listen(3000, () => {
     console.log('listening');
